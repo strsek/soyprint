731796d1e0a0813121f04a5b1eb83ce9d7189c1d
Stefan Trsek <str@live.at>
2022-02-09 13:12:34 +0000

    adaption to hybrid footprints

--- R/17_probability_maps.R
+++ R/17_probability_maps.R
@@ -17,7 +17,19 @@ library(viridis)
 regions <- fread("input_data/FABIO/inst/regions_full.csv")
 
 # load production footprints
-P <- readRDS("results/P.rds")
+P_mass  <- readRDS("results/P_mass.rds")
+P_value <- readRDS("results/P_value.rds")
+
+# append food/nonfood to colnames
+colnames(P_mass$A) <- paste0(colnames(P_mass$A),"_food")
+colnames(P_mass$B) <- paste0(colnames(P_mass$B),"_nonfood")
+colnames(P_value$A) <- paste0(colnames(P_value$A),"_food")
+colnames(P_value$B) <- paste0(colnames(P_value$B),"_nonfood")
+
+# bind
+P_mass <- cbind(P_mass$A, P_mass$B)
+P_value <- cbind(P_value$A, P_value$B)
+
 
 # load MU polygons and project to WGS84
 GEO_MUN_SOY <- readRDS("intermediate_data/GEO_MUN_SOY_fin.rds") %>% st_transform(crs = 4326)
@@ -28,19 +40,30 @@ is.finite.data.frame <- function(obj){
 }
 
 # join production footprints for soybeans with MU polygons
-P_soy_mun <- P[as.numeric(sub("_.*", "", rownames(P)))>1000 & sub(".*_", "", rownames(P)) == "c021",]
-P_soy_mun <- as.data.frame(as.matrix(P_soy_mun))
-# transform into integer probabilities
-P_soy_mun <- P_soy_mun/GEO_MUN_SOY$prod_bean # make sure they sum up to one!
-P_soy_mun[!is.finite(P_soy_mun)] <- 0
-P_soy_mun <- round(P_soy_mun*100)
-# change column names to ISO codes
-colnames(P_soy_mun) <- regions$iso3c[match(colnames(P_soy_mun), regions$code)]
-# join with polygons
-P_soy_mun <- mutate(P_soy_mun, co_mun = as.numeric(sub("_.*", "", rownames(P_soy_mun)))) %>% relocate(co_mun)
-GEO_MUN_P <- dplyr::select(GEO_MUN_SOY, c(co_mun:nm_state, prod_bean)) %>%
-  left_join(P_soy_mun, by = "co_mun") # or simply to cbind, as rows already math (but this is more safe)
+P_mun_mass <- P_mass[as.numeric(sub("_.*", "", rownames(P_mass)))>1000 & sub(".*_", "", rownames(P_mass)) == "c021",]
+P_mun_mass <- as.data.frame(as.matrix(P_mun_mass))
+P_mun_value <- P_value[as.numeric(sub("_.*", "", rownames(P_value)))>1000 & sub(".*_", "", rownames(P_value)) == "c021",]
+P_mun_value <- as.data.frame(as.matrix(P_mun_value))
 
+# transform into integer probabilities
+P_mun_mass <- P_mun_mass/GEO_MUN_SOY$prod_bean # make sure they sum up to one!
+P_mun_mass[!is.finite(P_mun_mass)] <- 0
+P_mun_mass <- round(P_mun_mass*100)
+P_mun_value <- P_mun_value/GEO_MUN_SOY$prod_bean # make sure they sum up to one!
+P_mun_value[!is.finite(P_mun_value)] <- 0
+P_mun_value <- round(P_mun_value*100)
+
+# change column names to ISO codes
+#colnames(P_mun_mass) <- regions$iso3c[match(colnames(P_mun_mass), regions$code)]
+
+# join with polygons
+P_mun_mass <- mutate(P_mun_mass, co_mun = as.numeric(sub("_.*", "", rownames(P_mun_mass)))) %>% relocate(co_mun)
+GEO_MUN_P_mass <- dplyr::select(GEO_MUN_SOY, c(co_mun:nm_state, prod_bean)) %>%
+  left_join(P_mun_mass, by = "co_mun") # or simply to cbind, as rows already math (but this is more safe)
+
+P_mun_value <- mutate(P_mun_value, co_mun = as.numeric(sub("_.*", "", rownames(P_mun_value)))) %>% relocate(co_mun)
+GEO_MUN_P_value <- dplyr::select(GEO_MUN_SOY, c(co_mun:nm_state, prod_bean)) %>%
+  left_join(P_mun_value, by = "co_mun") # or simply to cbind, as rows already math (but this is more safe)
 
 # prepare land-use tiles --------------------------------------
 
@@ -80,25 +103,48 @@ burn_rast <- function(rast, poly, class, value, file, zeroes = TRUE, subarea = F
 # prob_tile1 <- burn_rast(rast = tiles[[1]], poly = GEO_MUN_SOY, value = "co_state", class = 1, file = "prob_tile1.tif")
 # mapview(prob_tile1)
 
-# select final demand region
-reg <- "CHN"
+for (reg in c("EU")) {
+  for(type in c("food", "nonfood")) {
+    for(alloc in c("mass", "value")){
+
+# # select final demand region
+# reg <- "CHN"
+# # select foo/nonfood
+# type <- "food"
+# #select allocation method
+# alloc <- "value"
+
+geo <- if(alloc == "mass") GEO_MUN_P_mass else GEO_MUN_P_value
+
+if(reg == "EU") {
+  EU <- c("AUT", "BGR", "DNK", "FIN", "FRA", "DEU", "GRC", "HUN", "HRV", "IRL", "ITA", "MLT", "NLD", "CZE", "POL", "PRT", "ROU", "SVN", "SVK", "ESP", "SWE", "GBR", "BEL", "LUX", "LVA", "LTU", "EST", "CYP")
+  names(geo)[sub("_.*", "", names(geo)) %in% EU] <- paste0("EU_", sub(".*_", "", names(geo)[sub("_.*", "", names(geo)) %in% EU]))
+  mat <- as.matrix(st_drop_geometry(geo)[,which(names(st_drop_geometry(geo)) =="ARM_food"):ncol(st_drop_geometry(geo))])
+  sum_mat <- as(sapply(unique(colnames(mat)),"==",colnames(mat)), "Matrix")*1
+  mat <- mat %*% sum_mat
+  geo <- bind_cols(geo[,1:(which(names(geo) =="ARM_food")-1)], as.data.frame(as.matrix(mat)))
+}
+
 # create directory
-ifelse(!dir.exists(paste0("results/mb_tiles/",reg)), dir.create(paste0("results/mb_tiles/",reg)), "Folder exists already")
+dir <- paste0("results/mb_tiles/",reg,"_",type,"_",alloc)
+ifelse(!dir.exists(dir), dir.create(dir, recursive = TRUE), "Folder exists already")
+target <- paste0(reg,"_",type)
 
 # apply to tile list
 system.time(
 prob_tiles <- mclapply(names(tiles), function(x) {
   burn_rast(rast = tiles[[x]], 
-            poly = GEO_MUN_P, 
-            value = reg, class = 1, zeroes = FALSE,
-            file = paste0("results/mb_tiles/",reg,"/",x))
+            poly = geo, 
+            value = target, class = 1, zeroes = FALSE,
+            file = paste0(dir,"/",x))
   }, mc.cores = 12)
 )
 
 # build a vrt
-gdalUtilities::gdalbuildvrt(gdalfile = sapply(prob_tiles, filename), output.vrt = paste0("results/mb_tiles/",reg,"/prob.vrt"))
-prob_vrt <- raster(paste0("results/mb_tiles/",reg,"/prob.vrt"))
-mapview(prob_vrt, na.color = "transparent")
+gdalUtilities::gdalbuildvrt(gdalfile = sapply(prob_tiles, filename), output.vrt = paste0(dir,"/prob.vrt"))
+prob_vrt <- raster(paste0(dir,"/prob.vrt"))
+#mapview(prob_vrt, na.color = "transparent")
+
 
 # resample: two options
 
@@ -141,12 +187,12 @@ mapview(prob_vrt, na.color = "transparent")
 
 # 2: directly build a vrt from high-res tiles with desired resolution:
 gdalUtilities::gdalbuildvrt(gdalfile = sapply(prob_tiles, filename), 
-                                             output.vrt = paste0("results/mb_tiles/",reg,"/prob_agg.vrt"),
+                                             output.vrt = paste0(dir, "/prob_agg.vrt"),
                                              r = "nearest", # TODO: what resampling method?
                                              tr = res(prob_tiles[[1]])*16)
 
-prob_agg_vrt <- raster(paste0("results/mb_tiles/",reg,"/prob_agg.vrt"))
-mapview(prob_agg_vrt, na.color = "transparent")# plot
+prob_agg_vrt <- raster(paste0(dir,"/prob_agg.vrt"))
+#mapview(prob_agg_vrt, na.color = "transparent")# plot
 
 
 # plot -----------------------------------------
@@ -176,8 +222,8 @@ gplot_data <- function(x, maxpixels = 500000)  {
 prob_dat <- gplot_data(prob_agg_vrt, maxpixels = ncell(prob_agg_vrt)) %>% dplyr::filter(!is.na(value))
 
 # plot
-ggplot() +
-  geom_sf(data = GEO_states, fill = "gray19", color = "lightgrey", size = 0.1) +
+(prob_map <- ggplot() +
+  geom_sf(data = GEO_states, fill = "gray19", color = "lightgrey", size = 0.1) + # 
   geom_tile(data = dplyr::filter(prob_dat, !is.na(value)), 
             aes(x = x, y = y, fill = value) ) +
   #scale_fill_gradient("Land use\nprobability",
@@ -185,7 +231,18 @@ ggplot() +
   #                    na.value = NA) +
   scale_fill_viridis()+
   coord_sf(datum = sf::st_crs(prob_agg_vrt)) +
-  theme_void()
+  labs(fill = "probability", title = paste(reg, type, "consumption: land-use probability,", alloc, "allocation")) + 
+  theme_void()+
+  theme(plot.title = element_text(hjust = 0.5, size = 10), 
+        plot.margin = margin(t = -0.0, r = -0.2, b = -0.1, l = -0.2, "cm"),
+        legend.margin=margin(0,0,0,0), 
+        legend.box.margin=margin(t=0,r=0,b= 0,l=-60))
 #coord_quickmap()
+)
   
-  
+ggsave(filename = paste0(dir,"/prob_map_",reg,"_",type,"_",alloc,".png"), prob_map, width = 12, height = 10, units = "cm", scale = 2)  
+
+
+    }
+  }
+}
