359bd8ecf45ffce1401d447ad5249c4cce0f7403
Stefan Trsek <str@live.at>
2022-05-12 07:51:16 +0000

    - add ISO3 codes - compute consumer country/product-triggered footprints

--- R/16_footrpints.R
+++ R/16_footrpints.R
@@ -2,6 +2,7 @@
 
 library(Matrix)
 library(data.table)
+library(countrycode)
 
 LA_mass <- readRDS("intermediate_data/FABIO/2013_L_mass.rds")
 LB_mass <- readRDS("intermediate_data/FABIO/2013_B_inv_mass.rds")
@@ -21,6 +22,8 @@ l_mun <- read.csv("input_data/soy_areas_mapbiomas.csv")
 cbs <- readRDS("intermediate_data/FABIO/cbs_final.rds")
 areas <- unique(cbs[,.(area_code, area)])
 areas_mun <- areas[area_code > 1000,]
+regions <- fread("input_data/FABIO/inst/regions_full.csv")
+
 
 # prepare land-use data ------------------------------------------------------------------------
 
@@ -58,20 +61,26 @@ setkey(E, area_code, comm_code)
 # aggregate final demand categories
 # for YA
 colnames(YA) <- sub("_.*", "", colnames(YA))
+colnames(YA) <-  regions$iso3c[match(as.numeric(colnames(YA)), regions$code)] # change to ISO code
 sum_mat <- as(sapply(unique(colnames(YA)),"==",colnames(YA)), "Matrix")*1
-YA_agg <- YA %*% sum_mat 
+YA_country <- YA %*% sum_mat 
 # for YB
-dimnames(YB) <- list(paste0(IO.codes$Country.Code,"_",IO.codes$Product.Code),Y.codes$`Region Name`)
+# convert ISO2 codes to ISO3
+Y.codes$ISO3 <- countrycode(Y.codes$`Region Name`, origin = "iso2c", destination = "iso3c")
+Y.codes$ISO3[substr(Y.codes$`Region Name`,1,1) == "W"] <- paste0("ROW_",Y.codes$`Region Name`[substr(Y.codes$`Region Name`,1,1) == "W"] )
+dimnames(YB) <- list(paste0(IO.codes$Country.Code,"_",IO.codes$Product.Code),Y.codes$ISO3)
 sum_mat <- as(sapply(unique(colnames(YB)),"==",colnames(YB)), "Matrix")*1
-YB_agg <- YB %*% sum_mat 
+YB_country <- YB %*% sum_mat 
   
-# calculate production embodied in final demand impulse
-PA_mass  <- LA_mass  %*% YA_agg
-PA_value <- LA_value %*% YA_agg
-PB_mass  <- LB_mass  %*% YB_agg
-PB_value <- LB_value %*% YB_agg
+## by consumer country: 
 
-# calculate municipal land-use footprints
+# calculate production embodied in final demand impulse 
+PA_mass  <- LA_mass  %*% YA_country
+PA_value <- LA_value %*% YA_country
+PB_mass  <- LB_mass  %*% YB_country
+PB_value <- LB_value %*% YB_country
+
+# calculate municipal land-use footprints by country
 l <- as.vector(E$landuse / X)
 l[!is.finite(l)] <- 0
 FA_mass  <- l*PA_mass
@@ -79,14 +88,41 @@ FA_value <- l*PA_value
 FB_mass  <- l*PB_mass 
 FB_value <- l*PB_value
 
-P_mass  <- list(PA_mass,  PB_mass)
-P_value <- list(PA_value, PB_value)
-F_mass  <- list(FA_mass,  FB_mass)
-F_value <- list(FA_value, FB_value)
+
+## by consumer product: 
+YA_product <- as(diag(rowSums(YA_country)), "sparseMatrix")
+dimnames(YA_product) <- list(rownames(YA_country), sub(".*_", "", rownames(YA_country)))
+sum_mat <- as(sapply(unique(colnames(YA_product)),"==",colnames(YA_product)), "Matrix")*1
+YA_product <- YA_product %*% sum_mat
+YB_product <- as(diag(rowSums(YB_country)), "sparseMatrix")
+dimnames(YB_product) <- list(rownames(YB_country), sub(".*_", "", rownames(YB_country)))
+sum_mat <- as(sapply(unique(colnames(YB_product)),"==",colnames(YB_product)), "Matrix")*1
+YB_product <- YB_product %*% sum_mat
+
+# calculate production embodied in final demand impulse 
+PA_mass_product <-  LA_mass  %*% YA_product
+PA_value_product <- LA_value %*% YA_product
+PB_mass_product <-  LB_mass  %*% YB_product
+PB_value_product <- LB_value %*% YB_product
+
+# calculate municipal land-use footprints by country
+l <- as.vector(E$landuse / X)
+l[!is.finite(l)] <- 0
+FA_mass_product  <-  l*PA_mass_product
+FA_value_product <- l*PA_value_product
+FB_mass_product  <-  l*PB_mass_product 
+FB_value_product <- l*PB_value_product
+
+
+P_mass  <- list("A_country" = PA_mass,  "B_country" = PB_mass,  "A_product" = PA_mass_product, "B_product" =  PB_mass_product)
+P_value <- list("A_country" = PA_value, "B_country" = PB_value, "A_product" = PA_value_product, "B_product" = PB_value_product)
+F_mass  <- list("A_country" = FA_mass,  "B_country" = FB_mass,  "A_product" = FA_mass_product,  "B_product" = FB_mass_product)
+F_value <- list("A_country" = FA_value, "B_country" = FB_value, "A_product" = FA_value_product, "B_product" = FB_value_product)
 
 # Store results -----------------------
 saveRDS(P_mass , "results/P_mass.rds")
 saveRDS(P_value, "results/P_value.rds")
 saveRDS(F_mass , "results/F_mass.rds")
 saveRDS(F_value, "results/F_value.rds")
+
 
