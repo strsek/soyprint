703281caea97733c080630521b2c3a45f1b939cc
Stefan Trsek <str@live.at>
2022-05-12 07:59:37 +0000

    add experimental plot for sensitivity results

--- R/flow_maps.R
+++ R/flow_maps.R
@@ -7,7 +7,7 @@ library(ggthemes)
 library(viridis)
 library(ggforce)
 
-flows <- readRDS("intermediate_data/transport_flows.rds")
+flows <-readRDS("intermediate_data/flows_mu_comp.rds") #readRDS("intermediate_data/transport_flows.rds")
 SOY_MUN <- readRDS("intermediate_data/SOY_MUN_fin.rds")
 GEO_MUN_SOY <- readRDS("intermediate_data/GEO_MUN_SOY_fin.rds")
 MUN_capitals <- readRDS("intermediate_data/MUN_capitals.rds")
@@ -21,17 +21,24 @@ flows <- flows %>% left_join(MUN_capitals_df[,c(1,3:4)], by = c("co_orig" = "co_
 flows <- flows %>% left_join(MUN_capitals_df[,c(1,3:4)], by = c("co_dest" = "co_mun")) %>% rename("dest_lon" = "LONG", "dest_lat" = "LAT")
 
 # summarize modes and create separate frames for each product
-flows <- flows %>% group_by(co_orig,co_dest,product,orig_lon,orig_lat,dest_lon,dest_lat) %>% summarise(value = sum(value, na.rm = T), .groups = "drop")
+#flows <- flows %>% group_by(co_orig,co_dest,product,orig_lon,orig_lat,dest_lon,dest_lat) %>% summarise(value = sum(value, na.rm = T), .groups = "drop")
 flows_bean <- filter(flows, product == "bean")
 flows_oil <- filter(flows, product == "oil")
 flows_cake <- filter(flows, product == "cake")
 
 # check aggregate flow data
-flows_agg <- group_by(flows, co_orig, product) %>% summarise(value = sum(value, na.rm = T), .groups = "drop")
+flows_agg <- group_by(flows, co_orig, product) %>% summarise(across(euclid:mean, sum), .groups = "drop")
+flows_agg_wide <- pivot_wider(flows_agg, id_cols = c(co_orig), names_from = product, values_from = c(euclid, mean))
+flows_agg_wide[is.na(flows_agg_wide)] <- 0
+flows_agg <- flows_agg %>% mutate(diff_euclid_mean = euclid - mean)
+
+SOY_MUN_flows <- SOY_MUN %>% dplyr::select(c(co_mun:nm_state, starts_with("excess_supply"))) %>% left_join(flows_agg_wide, by = c("co_mun" = "co_orig"))
 
 # plot with ggplot
 
-flow <- flows_bean
+# general plots by product
+
+prod_flow_plots <- lapply(list(bean = flows_bean, oil = flows_oil, cake = flows_cake), function(flow){
 
 ggplot(data = states) +
   geom_sf(color = "grey", fill= "transparent", size = 0.1)+
@@ -43,7 +50,7 @@ ggplot(data = states) +
   #geom_link(data = flows_bean[which(flows_bean$value>10),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value, colour = stat(index)), lineend = "round")+
   #scale_colour_gradient(low = "red", high = "green") +
   #Here is the magic bit that sets line transparency - essential to make the plot readable
-  scale_alpha_continuous(range = c(0.05, 0.45), guide = "none")+
+  scale_alpha_continuous(range = c(0.1, 0.45), guide = "none")+
   #Set black background, ditch axes and fix aspect ratio
   #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
   #theme(axis.text = element_blank(),
@@ -57,58 +64,100 @@ ggplot(data = states) +
 
 ggsave(filename = "flows_bean.png")
 
+})
 
-flow <- flows_oil
+#flow <- flows_oil
+#
+#ggplot(data = states) +
+#  geom_sf(color = "grey", fill= "transparent", size = 0.1)+
+#  geom_sf(data = GEO_MUN_SOY[GEO_MUN_SOY$excess_supply_oil>0,], aes(fill = excess_supply_oil), color = "transparent", size = 0.01)+
+#  scale_fill_viridis(direction = -1, option = "A", na.value = "transparent", name = "Excess soy oil supply \n in tons")+
+#  #scale_fill_gradient(low = "white", high = "darkgreen", na.value = "transparent", name = "Excess soybean supply")+
+#  #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below
+#  geom_segment(data = flow[which(flow$value>1000),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value), col="firebrick")+
+#  #scale_colour_gradient(low = "red", high = "green") +
+#  #Here is the magic bit that sets line transparency - essential to make the plot readable
+#  scale_alpha_continuous(range = c(0.05, 0.45), guide = "none")+
+#  #Set black background, ditch axes and fix aspect ratio
+#  #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
+#  #theme(axis.text = element_blank(),
+#  #      axis.text.y = element_blank(),
+#  #      axis.ticks = element_blank(),
+#  #      rect = element_blank(),
+#  #      panel.background = element_rect(fill='darkgrey',colour='darkgrey'))+
+#  theme_map()+
+#  #theme(legend.position = "none")+
+#  coord_sf(crs = st_crs("+init=epsg:4326"))
+#
+#ggsave(filename = "flows_oil.png")
+#
+#
+#flow <- flows_cake
+#
+#ggplot(data = states) +
+#  geom_sf(color = "grey", fill= "transparent", size = 0.1)+
+#  geom_sf(data = GEO_MUN_SOY[GEO_MUN_SOY$excess_supply_cake>0,], aes(fill = excess_supply_cake), color = "transparent", size = 0.01)+
+#  scale_fill_viridis(direction = -1, option = "C", na.value = "transparent", name = "Excess soy cake supply \n in tons")+
+#  #scale_fill_gradient(low = "white", high = "darkgreen", na.value = "transparent", name = "Excess soybean supply")+
+#  #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below
+#  geom_segment(data = flow[which(flow$value>1000),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value), col="purple4")+
+#  #geom_link(data = flows_bean[which(flows_bean$value>10),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value, colour = stat(index)), lineend = "round")+
+#  #scale_colour_gradient(low = "red", high = "green") +
+#  #Here is the magic bit that sets line transparency - essential to make the plot readable
+#  scale_alpha_continuous(range = c(0.05, 0.45), guide = "none")+
+#  #Set black background, ditch axes and fix aspect ratio
+#  #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
+#  #theme(axis.text = element_blank(),
+#  #      axis.text.y = element_blank(),
+#  #      axis.ticks = element_blank(),
+#  #      rect = element_blank(),
+#  #      panel.background = element_rect(fill='darkgrey',colour='darkgrey'))+
+#  theme_map()+
+#  #theme(legend.position = "none")+
+#  coord_sf(crs = st_crs("+init=epsg:4326"))
+#
+#ggsave(filename = "flows_cake.png")
 
-ggplot(data = states) +
-  geom_sf(color = "grey", fill= "transparent", size = 0.1)+
-  geom_sf(data = GEO_MUN_SOY[GEO_MUN_SOY$excess_supply_oil>0,], aes(fill = excess_supply_oil), color = "transparent", size = 0.01)+
-  scale_fill_viridis(direction = -1, option = "A", na.value = "transparent", name = "Excess soy oil supply \n in tons")+
-  #scale_fill_gradient(low = "white", high = "darkgreen", na.value = "transparent", name = "Excess soybean supply")+
-  #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below
-  geom_segment(data = flow[which(flow$value>1000),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value), col="firebrick")+
-  #scale_colour_gradient(low = "red", high = "green") +
-  #Here is the magic bit that sets line transparency - essential to make the plot readable
-  scale_alpha_continuous(range = c(0.05, 0.45), guide = "none")+
-  #Set black background, ditch axes and fix aspect ratio
-  #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
-  #theme(axis.text = element_blank(),
-  #      axis.text.y = element_blank(),
-  #      axis.ticks = element_blank(),
-  #      rect = element_blank(),
-  #      panel.background = element_rect(fill='darkgrey',colour='darkgrey'))+
-  theme_map()+
-  #theme(legend.position = "none")+
-  coord_sf(crs = st_crs("+init=epsg:4326"))
 
-ggsave(filename = "flows_oil.png")
+# compare simulation mean with cv
+flow <- flows_bean
 
+flow_list <- list(bean = flows_bean, oil = flows_oil, cake = flows_cake)
 
-flow <- flows_cake
+prod_sensi_plots <- lapply(c("bean", "oil", "cake"), function(prod){
 
-ggplot(data = states) +
-  geom_sf(color = "grey", fill= "transparent", size = 0.1)+
-  geom_sf(data = GEO_MUN_SOY[GEO_MUN_SOY$excess_supply_cake>0,], aes(fill = excess_supply_cake), color = "transparent", size = 0.01)+
-  scale_fill_viridis(direction = -1, option = "C", na.value = "transparent", name = "Excess soy cake supply \n in tons")+
-  #scale_fill_gradient(low = "white", high = "darkgreen", na.value = "transparent", name = "Excess soybean supply")+
-  #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below
-  geom_segment(data = flow[which(flow$value>1000),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value), col="purple4")+
-  #geom_link(data = flows_bean[which(flows_bean$value>10),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=value, colour = stat(index)), lineend = "round")+
-  #scale_colour_gradient(low = "red", high = "green") +
-  #Here is the magic bit that sets line transparency - essential to make the plot readable
-  scale_alpha_continuous(range = c(0.05, 0.45), guide = "none")+
-  #Set black background, ditch axes and fix aspect ratio
-  #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
-  #theme(axis.text = element_blank(),
-  #      axis.text.y = element_blank(),
-  #      axis.ticks = element_blank(),
-  #      rect = element_blank(),
-  #      panel.background = element_rect(fill='darkgrey',colour='darkgrey'))+
-  theme_map()+
-  #theme(legend.position = "none")+
-  coord_sf(crs = st_crs("+init=epsg:4326"))
+  flow <- flow_list[[prod]]  
+  ggplot(data = states) +
+    geom_sf(color = "grey", fill= "transparent", size = 0.1)+
+    geom_sf(data = GEO_MUN_SOY[SOY_MUN[paste0("excess_supply_",prod)]>0,], aes(fill = !! sym(paste0("excess_supply_", prod))), color = "transparent", size = 0.01, alpha = 0.9)+
+    #scale_fill_viridis(direction = -1, option = "D", na.value = "transparent", name = "Excess soybean supply \n in tons")+
+    scale_fill_gradient(na.value = "transparent", low = "transparent", high = "darkgrey",  guide = "none") +
+    #The next line tells ggplot that we wish to plot line segments. The "alpha=" is line transparency and used below
+    geom_segment(data = flow[which(flow$mean > 0),], aes(x=orig_lon, y=orig_lat, xend=dest_lon, yend=dest_lat, alpha=mean, color = cv),
+               arrow = arrow(length = unit(0.005, "npc"), type = "closed"))+
+    #Here is the magic bit that sets line transparency - essential to make the plot readable
+    scale_alpha_continuous(range = c(0.01, 0.4), guide = "none")+
+    #scale_size_continuous(range = c(0.08, 1.3))+
+    #scale_color_viridis(option = "plasma") + 
+    scale_color_gradient(low = "#481567FF", high = "firebrick1") + 
+    #Set black background, ditch axes and fix aspect ratio
+    #theme(panel.background = element_rect(fill='transparent',colour='transparent'))+quiet+coord_equal()+
+    #theme(axis.text = element_blank(),
+    #      axis.text.y = element_blank(),
+    #      axis.ticks = element_blank(),
+    #      rect = element_blank(),
+    #      panel.background = element_rect(fill='darkgrey',colour='darkgrey'))+
+    theme_map()+
+    labs(fill = "excess supply") + 
+    #theme(legend.position = "none")+
+    theme(legend.position = "bottom") +
+    coord_sf(crs = st_crs("+init=epsg:4326"))
+  
+  #ggsave(filename = paste0("flowmap_sensi_",prod,".png"))
+  
+})
 
-ggsave(filename = "flows_cake.png")
+wrap_plots(prod_sensi_plots, guides = "collect") + theme(legend.position = "bottom")
 
 
 # general MU level plots
